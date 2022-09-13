180f14ed9ba3a7b09d7d76a90e3b455d08571523
LuanPatrik <72170325+LuanPatrik@users.noreply.github.com>
2022-09-13 08:33:01 -0300

    Update-Routes

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Control Finance!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Routes/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f06292',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Cadastre.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, TextInput, Text, StyleSheet } from 'react-native';
+import colors from '../global/color'
+
+export default function Cadastre() {
+ return (
+    <View>
+        <Text>Cadastro de Usário</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors('greenPrimary'),
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
