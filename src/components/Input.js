fa5951d56d66d9b8921a5fbbe04dfd2513fc3f79
José Guilherme <guilhermeverissimo14@gmail.com>
2022-09-20 11:52:52 -0300

    atualizando

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { TextInput, StyleSheet, KeyboardAvoidingView, Platform, } from 'react-native';
+import { View, TextInput, Image,StyleSheet, KeyboardAvoidingView, Platform, } from 'react-native';
 import colors from '../global/color';
 
-export default function Input({ placeholder, security = false, keyboardType, autoCorrect = true, returnKeyType = "next" }) {
+export default function Input({ placeholder, security = false, keyboardType, autoCorrect = true, returnKeyType = "next", icon = null}) {
     return (
         <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
-    
+            <View style={styles.inputArea}>
+            {icon === "user" && (
+                <Image
+                    source={require('../assets/login.png')}
+                    resizeMode="contain"
+                    // style={{ width: '15%', height: '100%' }}
+                />
+            )}
             <TextInput
                 placeholder={placeholder}
                 placeholderTextColor={colors("black")}
@@ -15,21 +22,41 @@ export default function Input({ placeholder, security = false, keyboardType, aut
                 returnKeyType={returnKeyType}
                 style={styles.input}
             />
+            </View>
         </KeyboardAvoidingView>
     );
 }
 
 const styles = StyleSheet.create({
+    // input: {
+    //     width: "100%",
+    //     borderBottomWidth: 1,
+    //     borderColor: colors("primary"),
+    //     color: colors("black"),
+    //     fontSize: 16,
+    //     padding: 10,
+    //     marginBottom: 20,
+    //     backgroundColor: colors("white"),
+    //     borderRadius: 8,
+    // },
     input: {
-        width: "100%",
+        width: '80%',
+        fontSize: 16,
+        paddingHorizontal: 10,
+        
+    },
+    inputArea: {
+        flexDirection: 'row',
+        width: '100%',
+        // height: 40,
+        marginBottom: 20,
+        alignItems: 'center',
         borderBottomWidth: 1,
         borderColor: colors("primary"),
-        color: colors("white"),
-        fontSize: 16,
-        padding: 10,
-        marginBottom: 20,
+        color: colors("black"),
         backgroundColor: colors("white"),
         borderRadius: 8,
+        
+
     },
-    
 });
