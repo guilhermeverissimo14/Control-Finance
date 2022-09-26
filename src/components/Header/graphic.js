7147643d80685774811da79b0d65787bf9429555
José Guilherme <guilhermeverissimo14@gmail.com>
2022-09-26 18:44:41 -0300

    Adicionando a imagem do gráfico.

--- src/components/Header/graphic.js
+++ src/components/Header/graphic.js
@@ -8,7 +8,7 @@ export default function Graphic() {
         <View style={styles.container}>
             <View style={styles.principal}>
                 <Image
-                   source={require('../../assets/graphic.png')}
+                  source={require('../../assets/graphic.png')}
                    resizeMode="contain"  
                 />
             </View>
