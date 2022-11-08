fadd6bd28100170df1a9577a3f9dde1eb80886e2
LUAN PATRIK SILVA PINTO <0057627@IFMG.EDU.BR>
2022-11-08 11:17:07 -0300

    Finalização da tela de esqueceu sua senha

--- src/screens/Forget/Forget.js
+++ src/screens/Forget/Forget.js
@@ -1,35 +1,37 @@
+import { useNavigation } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { render } from 'react-dom';
 import { View, TextInput, Text, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Image, Keyboard, SafeAreaView } from 'react-native';
 import colors from '../../global/color';
 
-const Step1 = ({ email, setEmail, next }) => {
+const Step1 = ({ email, setEmail, previous, next }) => {
     return(
         <SafeAreaView style={styles.element}>
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-                <>
-                    <View style={styles.step}>
-                        <Text style={styles.text}>Informe seu e-mail:</Text>
-                        <TextInput
-                            placeholder={'Responda aqui...'}
-                            placeholderTextColor={colors("white")}
-                            keyboardType={'default'}
-                            returnKeyType={"next"}
-                            onChangeText={setEmail}
-                            value={email}
-                            style={styles.input}
+            <View style={styles.step}>
+                <Text style={styles.text}>Informe seu e-mail:</Text>
+                <TextInput
+                    placeholder={'Responda aqui...'}
+                    placeholderTextColor={colors("white")}
+                    keyboardType={'default'}
+                    returnKeyType={"next"}
+                    onChangeText={setEmail}
+                    value={email}
+                    style={styles.input}
+                />
+                <View style={styles.align}>
+                    <TouchableOpacity onPress={() => navigation.goBack()} style={styles.arrowLeft}>
+                        <Image
+                            source={require('../../assets/arrowLeft.png')}
                         />
-                    </View>
-                    <View style={styles.align}>
-                        <View />
-                        <TouchableOpacity onPress={next} style={styles.arrowRight1}>
-                            <Image
-                                source={require('../../assets/arrowRight.png')}
-                            />
-                        </TouchableOpacity>
-                    </View>
-                </>
-            </TouchableWithoutFeedback>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={next} style={styles.arrowRight}>
+                        <Image
+                            source={require('../../assets/arrowRight.png')}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
         </SafeAreaView>
     )
 }
@@ -44,8 +46,49 @@ const Step2 = ({ code, setCode, previous, next}) => {
                     placeholderTextColor={colors("white")}
                     keyboardType={'email-address'}
                     returnKeyType={"next"}
-                    onChangeText={setEmail}
-                    value={email}
+                    onChangeText={setCode}
+                    value={code}
+                    style={styles.input}
+                />
+                <View style={styles.align}>
+                    <TouchableOpacity onPress={previous} style={styles.arrowLeft}>
+                        <Image
+                            source={require('../../assets/arrowLeft.png')}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={next} style={styles.arrowRight}>
+                        <Image
+                            source={require('../../assets/arrowRight.png')}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const Step3 = ({ password, setPassword, confirmPassword, setConfirmPassword, previous, next}) => {
+    return (
+        <SafeAreaView style={styles.element}>
+            <View style={styles.step}>
+                <Text style={styles.text}>Informe sua nova senha:</Text>
+                <TextInput
+                    placeholder={'Responda aqui...'}
+                    placeholderTextColor={colors("white")}
+                    keyboardType={'email-address'}
+                    returnKeyType={"next"}
+                    onChangeText={setPassword}
+                    value={password}
+                    style={styles.input}
+                />
+                <Text style={styles.text}>Confirme sua nova senha:</Text>
+                <TextInput
+                    placeholder={'Responda aqui...'}
+                    placeholderTextColor={colors("white")}
+                    keyboardType={'email-address'}
+                    returnKeyType={"next"}
+                    onChangeText={setConfirmPassword}
+                    value={confirmPassword}
                     style={styles.input}
                 />
                 <View style={styles.align}>
@@ -66,9 +109,13 @@ const Step2 = ({ code, setCode, previous, next}) => {
 }
 
 export default function Forget() {
+    const navigation = useNavigation();
+
     const [value, setValue] = useState(1);
     const [email, setEmail] = useState('');
     const [code, setCode] = useState();
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     const previous = () => {
         setValue(value - 1);
@@ -81,12 +128,26 @@ export default function Forget() {
     return(
         <>
             <View style={styles.container}>
-                {value === 1 && (
-                    <Step1 email={email} setEmail={setEmail} next={next}/>
-                )}
-                {value === 2 && (
-                    <Step2 code={code} setCode={setCode} previous={previous} next={next}/>
-                )}
+                <>
+                    <View style={styles.exit}>
+                        <TouchableOpacity onPress={() => navigation.goBack()}>
+                            <Image  
+                                source={require('../../assets/exitForget.png')}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                    
+                    {value === 1 && (
+                        <Step1 email={email} setEmail={setEmail} previous={previous} next={next}/>
+                    )}
+                    {value === 2 && (
+                        <Step2 code={code} setCode={setCode} previous={previous} next={next}/>
+                    )}
+                    {value === 3 && (
+                        <Step3 password={password} confirmPassword={confirmPassword} setPassword={setPassword} setConfirmPassword={setConfirmPassword} previous={previous} next={next} />
+                    )}
+                </>
+                
             </View>
         </>
     );
@@ -105,6 +166,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     text: {
+        marginTop: 15,
         marginHorizontal: 30,
         color: colors('white'),
         fontSize: 18,
@@ -122,8 +184,19 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
     },
-    arrowRight1: {
-        marginLeft: '100%',
+    arrowLeft: {
+        marginTop: 15,
+        marginHorizontal: 30,
     },
-
+    arrowRight: {
+        marginTop: 15,
+        marginLeft: '71%',
+        marginHorizontal: 30,
+    },
+    exit: {
+        display: 'flex',
+       alignItems: 'flex-end',
+       marginTop: 18,
+       marginRight: 10
+    }
 });
