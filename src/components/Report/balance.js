ec0f59bf29e7ec23f76b3395167be8b7c8dc822a
LUAN PATRIK SILVA PINTO <0057627@IFMG.EDU.BR>
2022-12-08 11:37:16 -0300

    Exibir dados no console.log

--- src/components/Report/balance.js
+++ src/components/Report/balance.js
@@ -1,21 +1,65 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet,  FlatList} from 'react-native';
 import colors from '../../global/color';
+import firestore from '@react-native-firebase/firestore';
 
 export default function Report() {
+    const [data, setData] = useState([]);
+    const Item = ({title}) => (
+        <View>
+            <Text style={styles.text}> {title}</Text>
+            <Text style={styles.text}>{title}</Text>
+        </View>
+        
+    );
+
+    const renderItem = ({item}) => (
+        <Item title={title}/>
+    );
+
+    const getBalance = () => {
+        firestore()
+        .collection('capital')
+        .get()
+        .then((querySnapshot) => {
+            let d = [];
+            querySnapshot.forEach((doc) => {
+                console.log(doc.description, " => ", doc.data());
+                const balance = {
+                    description: doc.data().description,
+                    val: doc.data().val
+                };
+                d.push(balance);
+            });
+            setData(d);
+        })
+        .catch((e) => {
+            console.log('Erro: ' + e);
+        });
+    }
+
+    useEffect(() => {
+        getBalance();
+    }, []);
+
     return (
         <View style={styles.container}>
-            <View style={styles.item}>
+            <FlatList
+                data={data}
+                renderItem={renderItem}
+                keyExtractor={item => item.description}
+            />
+            {/* <View style={styles.item}>
                 <Text style={styles.text}> Sálario</Text>
                 <Text style={styles.text}> R$ 1.800,00</Text>
             </View>
             <View style={styles.line} />
             <View style={styles.item1}>
-                <Text style={styles.text}> Conta de luz</Text>
+                <Text style={styles.text}> Conta </Text>
                 <Text style={styles.text}> R$ 74,19</Text>
-            </View>
+            </View> */}
             <View style={styles.line} />
+           
         </View>
     );
 }
